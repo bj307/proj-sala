d66e39a35e67d8239865abfcc6ed617124efa1a7
bj307 <62263060+bj307@users.noreply.github.com>
2022-07-12 08:30:00 -0300

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,194 +1,168 @@
-/* Aula 20 MaiaQuiz  */
-
-let titulo = document.querySelector('h1')
-let instrucoes = document.querySelector('#instrucoes')
-let aviso = document.querySelector('#aviso')
-//let respostaEsta = document.querySelector('#respostaEsta')
-let pontos = 0 // pontos para o placar
-let placar = 0 // placar
-
-// PERGUNTA
-let numQuestao = document.querySelector('#numQuestao')
-let pergunta   = document.querySelector('#pergunta')
-
-// ALTERNATIVAS
-let a = document.querySelector('#a')
-let b = document.querySelector('#b')
-let c = document.querySelector('#c')
-
-// article com a class questoes
-let articleQuestoes = document.querySelector('.questoes')
-// ol li com as alternativas
-let alternativas = document.querySelector('#alternativas')
-
-const q0 = {
-    numQuestao   : 0,
-    pergunta     : "Pergunta",
-    alternativaA : "Alternativa A",
-    alternativaB : "Alternativa B",
-    alternativaC : "Alternativa C",
-    correta      : "0",
-}
-
-const q1 = {
-    numQuestao   : 1,
-    pergunta     : "Boreal é o mesmo que...",
-    alternativaA : "Sul",
-    alternativaB : "Leste",
-    alternativaC : "Norte",
-    correta      : "Norte",
-}
-
-const q2 = {
-    numQuestao   : 2,
-    pergunta     : "Austral é o mesmo que...",
-    alternativaA : "Oeste",
-    alternativaB : "Sul",
-    alternativaC : "Norte",
-    correta      : "Sul",
-}
-
-const q3 = {
-    numQuestao   : 3,
-    pergunta     : "Nascente é o mesmo que...",
-    alternativaA : "Sul",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Leste",
-}
-
-const q4 = {
-    numQuestao   : 4,
-    pergunta     : "Poente é o mesmo que...",
-    alternativaA : "Norte",
-    alternativaB : "Leste",
-    alternativaC : "Oeste",
-    correta      : "Oeste",
-}
-
-const q5 = {
-    numQuestao   : 5,
-    pergunta     : "O Brasil se localiza em qual continente?",
-    alternativaA : "África",
-    alternativaB : "Europa",
-    alternativaC : "América",
-    correta      : "América",
-}
-
-// CONSTANTE COM UM ARRAY DE OBJETOS COM TODAS AS QUESTOES
-const questoes = [q0, q1, q2, q3, q4, q5]
-
-let numero = document.querySelector('#numero')
-let total  = document.querySelector('#total')
-
-numero.textContent = q1.numQuestao
-
-let totalDeQuestoes = (questoes.length)-1
-console.log("Total de questões " + totalDeQuestoes)
-total.textContent = totalDeQuestoes
-
-// MONTAR A 1a QUESTAO COMPLETA, para iniciar o Quiz
-numQuestao.textContent = q1.numQuestao
-pergunta.textContent   = q1.pergunta
-a.textContent = q1.alternativaA
-b.textContent = q1.alternativaB
-c.textContent = q1.alternativaC
-
-// CONFIGURAR O VALUE INICIAL DA 1a QUESTAO COMPLETA
-a.setAttribute('value', '1A')
-b.setAttribute('value', '1B')
-c.setAttribute('value', '1C')
-
-// PARA MONTAR AS PROXIMAS QUESTOES
-function proximaQuestao(nQuestao) {
-    numero.textContent = nQuestao
-    numQuestao.textContent = questoes[nQuestao].numQuestao
-    pergunta.textContent   = questoes[nQuestao].pergunta
-    a.textContent = questoes[nQuestao].alternativaA
-    b.textContent = questoes[nQuestao].alternativaB
-    c.textContent = questoes[nQuestao].alternativaC
-    a.setAttribute('value', nQuestao+'A')
-    b.setAttribute('value', nQuestao+'B')
-    c.setAttribute('value', nQuestao+'C')
-}
-
-function bloquearAlternativas() {
-    a.classList.add('bloqueado')
-    b.classList.add('bloqueado')
-    c.classList.add('bloqueado')
-}
-
-function desbloquearAlternativas() {
-    a.classList.remove('bloqueado')
-    b.classList.remove('bloqueado')
-    c.classList.remove('bloqueado')
-}
-
-function verificarSeAcertou(nQuestao, resposta) {
-
-    let numeroDaQuestao = nQuestao.value
-    console.log("Questão " + numeroDaQuestao)
-
-    let respostaEscolhida = resposta.textContent
-    //console.log("RespU " + respostaEscolhida)
-
-    let certa = questoes[numeroDaQuestao].correta
-    //console.log("RespC " + certa)
-
-    if(respostaEscolhida == certa) {
-        //console.log("Acertou")
-        //respostaEsta.textContent = "Correta 😊"
-        pontos += 10 // pontos = pontos + 10
-    } else {
-        //console.log("Errou!")
-        //respostaEsta.textContent = "Errada 😢"
+(function() {
+  var questions = [{
+    question: "Questão 1",
+    choices: ["1", "2"]
+  }, {
+    question: "Questão 2",
+    choices: ["A", "B", "C"]
+  }];
+  
+  var questionCounter = 0; //segue numero de questões
+  var selections = []; //respostas do usuário Array
+  var quiz = $('#quiz'); //div da quiz
+  
+  //mostra questão inicial
+  displayNext();
+  
+  // Click handler para botão "proxima"
+  $('#next').on('click', function (e) {
+    e.preventDefault();
+    
+    // rava animações indevidas
+    if(quiz.is(':animated')) {        
+      return false;
     }
-
-    // atualizar placar
-    placar = pontos
-    instrucoes.textContent = "Pontos " + placar
-
-    // bloquear a escolha de opcoes
-    bloquearAlternativas()
-
-    setTimeout(function() {
-        //respostaEsta.textContent = '...'
-        proxima = numeroDaQuestao+1
-
-        if(proxima > totalDeQuestoes) {
-            console.log('Fim do Jogo!')
-            fimDoJogo()
-        } else {
-            proximaQuestao(proxima)
+    choose();
+    
+    // protecão caso não ecolha nenhuma questão
+    if (isNaN(selections[questionCounter])) {
+      alert('Please make a selection!');
+    } else {
+      questionCounter++;
+      displayNext();
+    }
+  });
+  
+  // Click handler para o botão 'voltar'
+  $('#prev').on('click', function (e) {
+    e.preventDefault();
+    
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    choose();
+    questionCounter--;
+    displayNext();
+  });
+  
+  // Click handler para o botão 'iniciar'
+  $('#start').on('click', function (e) {
+    e.preventDefault();
+    
+    if(quiz.is(':animated')) {
+      return false;
+    }
+    questionCounter = 0;
+    selections = [];
+    displayNext();
+    $('#start').hide();
+  });
+  
+  // animações de hover
+  $('.button').on('mouseenter', function () {
+    $(this).addClass('active');
+  });
+  $('.button').on('mouseleave', function () {
+    $(this).removeClass('active');
+  });
+  
+  // cria a div que contem a respostas
+  function createQuestionElement(index) {
+    var qElement = $('<div>', {
+      id: 'question'
+    });
+    
+    var header = $('<h2>Question ' + (index + 1) + ':</h2>');
+    qElement.append(header);
+    
+    var question = $('<p>').append(questions[index].question);
+    qElement.append(question);
+    
+    var radioButtons = createRadios(index);
+    qElement.append(radioButtons);
+    
+    return qElement;
+  }
+  
+  // cria a lista de respostas com radio buttons
+  function createRadios(index) {
+    var radioList = $('<ul>');
+    var item;
+    var input = '';
+    for (var i = 0; i < questions[index].choices.length; i++) {
+      item = $('<li>');
+      input = '<input type="radio" name="answer" value=' + i + ' />';
+      input += questions[index].choices[i];
+      item.append(input);
+      radioList.append(item);
+    }
+    return radioList;
+  }
+  
+  // pega o valor da resposta escolhida e coloca na array selections
+  function choose() {
+    selections[questionCounter] = +$('input[name="answer"]:checked').val();
+  }
+  
+  // busca e exibe o proximo elemento
+  function displayNext() {
+    quiz.fadeOut(function() {
+      $('#question').remove();
+      
+      if(questionCounter < questions.length){
+        var nextQuestion = createQuestionElement(questionCounter);
+        quiz.append(nextQuestion).fadeIn();
+        if (!(isNaN(selections[questionCounter]))) {
+          $('input[value='+selections[questionCounter]+']').prop('checked', true);
         }
-    }, 250)
-    desbloquearAlternativas()
-}
-
-function fimDoJogo() {
-    instrucoes.textContent = "Fim de Jogo!"
-    numQuestao.textContent = ""
-
-    let pont = ''
-    pontos == 0 ? pont = 'ponto' : pont = 'pontos'
-
-    pergunta.textContent   = "Você conseguiu " + pontos + " " + pont
-
-    aviso.textContent = "Você conseguiu " + pontos + " " + pont
-
-    a.textContent = ""
-    b.textContent = ""
-    c.textContent = ""
-
-    a.setAttribute('value', '0')
-    b.setAttribute('value', '0')
-    c.setAttribute('value', '0')
-
-    // OCULTAR O ARTICLE DA QUESTAO
-    articleQuestoes.style.display = 'none'
-
-    setTimeout(function() {
-        pontos = 0 // zerar placar
-        location.reload();
-    }, 2000)
-}
+        
+        // controles para o botão voltar aparecer após a primeira questão
+        if(questionCounter === 1){
+          $('#prev').show();
+        } else if(questionCounter === 0){
+          
+          $('#prev').hide();
+          $('#next').show();
+        }
+      }else {
+        var scoreElem = displayScore();
+        quiz.append(scoreElem).fadeIn();
+        $('#next').hide();
+        $('#prev').hide();
+        $('#start').show();
+      }
+    });
+  }
+  
+  // Analisa as resposta e retorna o resultado
+  function displayScore() {
+    var score = $('<p>',{id: 'question'});
+    var matchKit;
+    var selection = selections[0] +""+selections[1];
+      switch(selection) {
+    case "00":
+        matchKit = 'Resposta 1 e A';
+        break;
+    case "01":
+        matchKit = 'Resposta 1 e B';
+        break;    
+    case "02":
+        matchKit = 'Resposta 1 e C';
+        break;
+    case "10":
+        matchKit = 'Resposta 2 e A';
+        break;
+    case "11":
+        matchKit = 'Resposta 2 e B';
+        break;
+    case "12":
+        matchKit = 'Resposta 2 e C';
+        break;   
+    default:
+        matchKit = 'Sem resposta';
+  }
+    
+    score.append('O kit certo é o: ' + matchKit + ' !');
+    return score;
+  }
+})();
